7270acb670472d32779f763e9846ebf7c5ffc3ca
Pzainab <72129609+Pzainab@users.noreply.github.com>
2020-10-01 13:45:20 +0530

    Create sets.cpp

---
+++ sets.cpp
@@ -0,0 +1,4 @@
+template < class T,                        // set::key_type/value_type
+           class Compare = less<T>,        // set::key_compare/value_compare
+           class Alloc = allocator<T>      // set::allocator_type
+           > class set;
